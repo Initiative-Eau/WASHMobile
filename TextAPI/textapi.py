03d7268228dc8fb397432aa89111c95082fc053c
mfrederikse <mjfrederikse@gmail.com>
2017-06-29 19:21:14 +0000

    security loophole closed

--- TextAPI/textapi.py
+++ TextAPI/textapi.py
@@ -6,8 +6,8 @@ import os
 water_sources_df = pandas.read_csv(os.path.join(os.getcwd(), "water_sources_data.csv"))
 print("loaded_data")
 
-account_sid = "AC36878ead4766e4b1ebd76dd4ae4ded4a"
-auth_token = "9084ae0bf6b06a52fd5c3b102d57a515"
+account_sid = "<account>"
+auth_token = "<auth>"
 client = Client(accound_sid, auth_token)
 
 message = client.api.account.messages.create(to="+12018741921",from_="+<account phone number>", body="What's Gucci Fam?")
