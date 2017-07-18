1d59e7f33a436f51ca488044eb15fe15eedcdfad
mfrederikse <mjfrederikse@gmail.com>
2017-07-18 05:49:51 +0000

    webpage improvements

--- Webpage/launch_page.py
+++ Webpage/launch_page.py
@@ -1,50 +1,66 @@
 from flask import Flask, render_template
-from flask_googlemaps import GoogleMaps, Map, icons
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map, icons
+import pandas
 import os
+import json
 
-app = Flask(__name__)
+water_sources_df = pandas.read_csv(os.path.join(os.getcwd(),"water_sources_data.csv"), header=0)
+water_source_markers = []
+icon = '//maps.google.com/mapfiles/ms/icons/blue-dot.png'
+for i in range(water_sources_df.shape[0]):
+    tmp = water_sources_df.loc[i]
+    data = {'icon': icon,
+            'title': tmp['ID'],
+            'lat': tmp['Latitude'],
+            'lng': tmp['Longitude'],
+            'infobox': (
+                "<h1>ID: <b style='color:blue;'>"+tmp['ID']+"</b></h1>"
+                "<h2>Quality: "+tmp['Grade']+" ("+tmp['WQI']+")</h2>"
+                "<h3>Lat: "+str(tmp['Latitude'])+" Lng: "+str(tmp['Longitude'])+" Updated: "+str(tmp['Updated'])+"</h3>"
+                "<img src='//placehold.it/50'>"
+                "<br>Location Image"
+            )}
+            # 'infobox': "ID: < b style='color:green;'>str(tmp['ID'])< / b >"}
 
-GoogleMaps(app, key = "AIzaSyBLZbOLB1YcrTlXz_3_-yMgU6MG04wp5lU")
+        # , WQI: "+str(tmp['WQI'])+", Grade: "+str(tmp['Grade'])+", Updated: "+str(tmp['Updated'])}
 
-@app.route("/WashMobile")
-def mapview():
-    trdmap = Map(
-        identifier="trdmap",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[
-            {
-                'icon': icons.alpha.B,
-                'lat':  37.4419,
-                'lng':  -122.1419,
-                'infobox': "Hello I am < b style='color:green;'>B< / b >!"
-            },
-            {
-                'icon': icons.dots.blue,
-                'lat': 37.4300,
-                'lng': -122.1400,
-                'infobox': "Hello I am < b style='color:blue;'>BLUE< / b >!"
-            },
-            {
-                'icon': icons.dots.yellow,
-                'lat': 37.4500,
-                'lng': -122.1350,
-                'infobox': (
-                    "Hello I am < b style='color:#ffcc00;'> YELLOW < / b >!"
-                    "< h2 >It is HTML title< / h2 >"
-                    "< img src=' //placehold.it/50' >"
-                    "< br >Images allowed!"
-                )
-            }
-        ]
+    water_source_markers.append(data)
+
+print(water_source_markers[0])
+
+app = Flask(__name__, template_folder="templates")
+
+# you can set key as config
+app.config['GOOGLEMAPS_KEY'] = "AIzaSyAZzeHhs-8JZ7i18MjFuM35dJHq70n3Hx4"
+
+# you can also pass key here
+GoogleMaps(app, key="AIzaSyAZzeHhs-8JZ7i18MjFuM35dJHq70n3Hx4")
+
+@app.route('/fullmap')
+def fullmap():
+    fullmap = Map(
+        identifier="fullmap",
+        varname="fullmap",
+        style=(
+            "height:75%;"
+            "width:75%;"
+            "top:10;"
+            "left:10;"
+            "position:absolute;"
+            "z-index:200;"
+        ),
+        lat=12.067543,
+        lng=0.359191,
+        markers=water_source_markers,
+        # maptype = "TERRAIN",
+        zoom="14"
     )
-    return render_template('map_templates.html', trdmap=trdmap)
-
-# Template:
-# in head:
-#     {{trdmap.js}}
-# in body:
-#     {{trdmap.html}}
+    return render_template('example_fullmap.html', fullmap=fullmap)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # TO RUN ON LOCAL MACHINE:
+    #app.run(debug=True, use_reloader=True)
+
+    # TO RUN ON GOOGLE CLOUD ENGINE:
+    app.run(host="0.0.0.0", port=80, debug=False)
